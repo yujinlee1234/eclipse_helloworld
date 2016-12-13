db71456891f4fed478002853ed41c2e3d106bea6
yujinlee1234 <godgod5328@gmail.com>
2016-12-13 15:20:28 +0900

    pushPrint.java에서 한줄추가

--- src/kr/or/dgit/bigdata/helloworld/PushPrint.java
+++ src/kr/or/dgit/bigdata/helloworld/PushPrint.java
@@ -3,5 +3,6 @@ package kr.or.dgit.bigdata.helloworld;
 public class PushPrint {
 	public static void main(String[] args) {
 		System.out.println("Push GitHub");
+		System.out.println("GitHub Write");
 	}
 }
