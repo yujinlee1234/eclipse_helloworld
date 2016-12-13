327b9f73a52a1aba8458dce7fe62c072d24fcd6e
yujinLee <godgod5328@gmail.com>
2016-12-13 15:21:09 +0900

    한줄추가

--- src/kr/or/dgit/bigdata/helloworld/PushPrint.java
+++ src/kr/or/dgit/bigdata/helloworld/PushPrint.java
@@ -3,5 +3,6 @@ package kr.or.dgit.bigdata.helloworld;
 public class PushPrint {
 	public static void main(String[] args) {
 		System.out.println("Push GitHub");
+		System.out.println("Local에서 한줄 추가");
 	}
 }
