87ba59ee106d7a23bf7639d129f844c8a822bc71
yujinLee <godgod5328@gmail.com>
2016-12-13 14:51:06 +0900

    HelloWorld.java 한 줄 추가

--- src/kr/or/dgit/bigdata/helloworld/HelloWorld.java
+++ src/kr/or/dgit/bigdata/helloworld/HelloWorld.java
@@ -3,6 +3,6 @@ package kr.or.dgit.bigdata.helloworld;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		
+		System.out.println("Hotfix에서 작업");
 	}
 }
