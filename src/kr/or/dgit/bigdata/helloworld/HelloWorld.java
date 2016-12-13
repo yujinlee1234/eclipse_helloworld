038909cc3b40715b0889981f5e5ed07bafc354cf
yujinLee <godgod5328@gmail.com>
2016-12-13 14:54:56 +0900

    의도적으로 한줄추가시켜 충돌유발

--- src/kr/or/dgit/bigdata/helloworld/HelloWorld.java
+++ src/kr/or/dgit/bigdata/helloworld/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hotfix에서 작업");
+		System.out.println("의도적 충돌 유도");
 	}
 }
