3fc39391e56ceba150fe7d0062901739f80e36ce
yujinLee <godgod5328@gmail.com>
2016-12-13 14:53:37 +0900

    master에서 추가

--- src/kr/or/dgit/bigdata/helloworld/HelloWorld.java
+++ src/kr/or/dgit/bigdata/helloworld/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hotfix에서 작업");
+		System.out.println("Master에서 추가");
 	}
 }
